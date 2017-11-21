0bd2205f9d9179f978dd3c03338cb4db52d9b424
BICESTER\IBurton <iburton@valueretail.com>
2017-11-21 12:34:21 +0000

    Made changes to main js file. spelling mistake, and nullcheck on local storage.

--- js/main.js
+++ js/main.js
@@ -10,7 +10,7 @@ function saveIssue(e){
     var issue = {
         id : issuedId,
         description : issueDesc,
-        severity : issuedSeverity,
+        severity : issueSeverity,
         assignedTo : issueAssignedTo,
         status : issueStatus
     }
@@ -30,31 +30,39 @@ function saveIssue(e){
     fetchIssues();
 
     e.preventDefault();
+	
 }
 
 
 
 function fetchIssues(){
     var issues = JSON.parse(localStorage.getItem('issues'));
-    var issuesliste = document.getElementById('issuesList');
-
+    var issuesList = document.getElementById('issuesList');
     issuesList.innerHTML = '';
+	// you need to check if the list of issues is null (or the localStorage)
+	if(issues != null && issues.length > 0){
 
-    for(var i = 0; i < issues.length; i++) {
-        var id = issues[i].id;
-        var desc = issues[i].description;
-        var severity = issues[i].severity;
-        var assignedTo = issues[i].assignedTo;
-        var status = issues[i].status;
+		for(var i = 0; i < issues.length; i++) {
+			var id = issues[i].id;
+			var desc = issues[i].description;
+			var severity = issues[i].severity;
+			var assignedTo = issues[i].assignedTo;
+			var status = issues[i].status;
 
-        issuesList.innerHTML += '<div class="well">'+
-                                '<h6>Issue ID: ' + id + '</h6>'+
-                                '<p><span class="label label-info">' + status + '</span></p>' +
-                                '<h3>' + desc + '</h3>'+
-                                '<p><span class="glyphicon glyphicon-time"></span>' + severity + '</p>' +
-                                '<p><span class="glyphicon glyphicon-user"></span>' + assignedTo + '</p>' +
-                                '<a href="#" onclick="setStatusClosed(\''+id+'\')" class="btn btn-warning">Close</a>' +
-                                '<a href="#" onclick="deleteIssue(\''+id+'\')" class="btn btn-danger">Delete</a>' +
-                                '</div>';
-    }
+			issuesList.innerHTML += '<div class="well">'+
+									'<h6>Issue ID: ' + id + '</h6>'+
+									'<p><span class="label label-info">' + status + '</span></p>' +
+									'<h3>' + desc + '</h3>'+
+									'<p><span class="glyphicon glyphicon-time"></span>' + severity + '</p>' +
+									'<p><span class="glyphicon glyphicon-user"></span>' + assignedTo + '</p>' +
+									'<a href="#" onclick="setStatusClosed(\''+id+'\')" class="btn btn-warning">Close</a>' +
+									'<a href="#" onclick="deleteIssue(\''+id+'\')" class="btn btn-danger">Delete</a>' +
+									'</div>';
+		}
+	}
+	else{
+		// you might want to display something friendly to the user to say that there are no issues.
+		// like maybe: 	issuesList.innerHTML += '<div class="well">There are no issues</div>';
+		console.log("There are no issues");
+	}
 }
